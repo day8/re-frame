6494bff3f6c4a91ec926e0732ff47c74c1b15a2a
hipitihop <anonymous.amphibian@gmail.com>
2015-02-10 10:56:55 +1000

    bump project dependencies [org.clojure/clojurescript "0.0-2760"] [reagent "0.5.0-alpha3"]

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url          "https://github.com/Day8/re-frame.git"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2665"]
+                 [org.clojure/clojurescript "0.0-2760"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha" :scope "provided"]
-                 [reagent "0.4.3"]
+                 [reagent "0.5.0-alpha3"]
                  [historian "1.0.7"]]
 
   :profiles     {:debug {:debug true}
