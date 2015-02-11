98985da5cbddd0aee6720320593a30908b78a745
mike-thompson-day8 <mike.thompson@day8.com.au>
2015-02-11 12:22:37 +1100

    Tidy up project.clj

--- project.clj
+++ project.clj
@@ -3,19 +3,22 @@
   :url          "https://github.com/Day8/re-frame.git"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2760"]
+                 [org.clojure/clojurescript "0.0-2760" :scope "provided"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha" :scope "provided"]
-                 [reagent "0.5.0-alpha3"]
+                 [reagent "0.5.0-alpha3" :scope "provided"]
                  [historian "1.0.7"]]
 
   :profiles     {:debug {:debug true}
                  :dev   {:dependencies [[spellhouse/clairvoyant "0.0-48-gf5e59d3"]]
 
-                         :plugins      [[lein-cljsbuild "1.0.3"]
-                                        [com.cemerick/clojurescript.test "0.3.3"]]}}
+                         :plugins      [[lein-cljsbuild "1.0.4"]
+                                        #_[com.cemerick/clojurescript.test "0.3.3" ]]}}
+
+
+  :clean-targets [:target-path
+                  "run/compiled/demo"]
 
   :resource-paths ["run/resources"]
-  ;:jvm-opts     ^:replace ["-Xms2g" "-Xmx2g" "-server"]
 
   :source-paths ["src"]
   :test-paths   ["test"]
@@ -23,18 +26,19 @@
   ;; Exclude the demo code from the output of either:
   ;;   - lein jar
   ;;   - lein install
-  :jar-exclusions [#"(?:^|\/)re-frame-demo\/"]
+  ;; :jar-exclusions [#"(?:^|\/)re-frame-demo\/"]
 
-  :cljsbuild    {:builds [{:id "demo"
-                           :source-paths   ["src" "src/re_frame"]
+  :cljsbuild    {:builds [;; currently bogus, there is no demo
+                          {:id "demo"
+                           :source-paths   ["src"]
                            :compiler       {:output-to     "run/compiled/demo.js"
                                             :source-map    "run/compiled/demo.js.map"
                                             :output-dir    "run/compiled/demo"
                                             :optimizations :none
-                                            :pretty-print  true}}
-                          ]}
+                                            :pretty-print  true}}]}
 
   :aliases      {"auto"  ["do" "clean," "cljsbuild" "clean," "cljsbuild" "auto" "demo,"]
                  "once"  ["do" "clean," "cljsbuild" "clean," "cljsbuild" "once" "demo,"]
-                 "test"  ["do" "clean," "cljsbuild" "clean," "cljsbuild" "auto" "test"]}
+                 ;"test"  ["do" "clean," "cljsbuild" "clean," "cljsbuild" "auto" "test"]
+                 }
   )
