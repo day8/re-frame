9f233f07b7869c020c912aff6478c9cda80f8918
mike-thompson-day8 <mike.thompson@day8.com.au>
2015-02-25 08:26:47 +1100

    Change warning

--- src/re_frame/subs.cljs
+++ src/re_frame/subs.cljs
@@ -1,6 +1,6 @@
 (ns re-frame.subs
  (:require
-  [re-frame.db :refer [app-db]]
+  [re-frame.db    :refer [app-db]]
   [re-frame.utils :refer [first-in-vector warn]]))
 
 
@@ -12,7 +12,7 @@
   "register a hander function for an id"
   [key-v handler-fn]
   (if (contains? @key->fn key-v)
-   (warn "re-frame: overwriting subscription-handler for: " key-v))   ;; allow it, but warn.
+    (warn "re-frame: overwriting subscription-handler for: " key-v))   ;; allow it, but warn.
   (swap! key->fn assoc key-v handler-fn))
 
 
@@ -20,7 +20,7 @@
   "returns a reagent/reaction which observes a part of the "
   [v]
   (let [key-v       (first-in-vector v)
-       handler-fn  (get @key->fn key-v)]
+        handler-fn  (get @key->fn key-v)]
     (if (nil? handler-fn)
-      (warn "re-frame: no subscription handler registered for: \"" key-v "\".  Returning nil.")
+      (warn "re-frame: no subscription handler registered for: \"" key-v "\".  Returning a nil subscription.")
       (handler-fn app-db v))))
