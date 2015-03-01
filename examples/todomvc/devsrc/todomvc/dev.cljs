90409377028b77a310f3f385bc46896b75f32c1b
mike-thompson-day8 <mike.thompson@day8.com.au>
2015-03-02 07:56:47 +1100

    Better loading of todomvc

--- examples/todomvc/devsrc/todomvc/dev.cljs
+++ examples/todomvc/devsrc/todomvc/dev.cljs
@@ -1,6 +1,10 @@
 (ns todomvc.dev
-  (:require [todomvc.core :as example]
+  (:require [todomvc.core :as todomvc]
             [figwheel.client :as fw]))
 
-(fw/start {:on-jsload example/run
+#_(fw/start {:on-jsload todomvc/main
            :websocket-url "ws://localhost:3449/figwheel-ws"})
+
+(defn start []
+  (fw/start {:jsload-callback (fn [] (todomvc/main))})
+  (todomvc/main))
