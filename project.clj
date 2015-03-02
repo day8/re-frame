894664ae20135f58388b36919ecbb08d955c83e5
mike-thompson-day8 <mike.thompson@day8.com.au>
2015-03-02 23:01:40 +1100

    Next release will be 0.2.0

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject re-frame "0.1.8"
+(defproject re-frame "0.2.0"
   :description  "A Reagent Framework For Writing SPAs, in Clojurescript"
   :url          "https://github.com/Day8/re-frame.git"
   :license      {:name "MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-
                  [org.clojure/clojurescript "0.0-2760"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [reagent "0.5.0-alpha3"]]
