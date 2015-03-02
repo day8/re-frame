bd492d12728b2a7c432b3eebbef73a08b323029d
mike-thompson-day8 <mike.thompson@day8.com.au>
2015-03-02 23:56:05 +1100

    Take out figwheel for the moment, it is annoying me.

--- examples/todomvc/devsrc/todomvc/dev.cljs
+++ examples/todomvc/devsrc/todomvc/dev.cljs
@@ -2,9 +2,6 @@
   (:require [todomvc.core :as todomvc]
             [figwheel.client :as fw]))
 
-#_(fw/start {:on-jsload todomvc/main
-           :websocket-url "ws://localhost:3449/figwheel-ws"})
-
 (defn start []
-  (fw/start {:jsload-callback (fn [] (todomvc/main))})
+  ;(fw/start {:jsload-callback (fn [] (todomvc/main))})
   (todomvc/main))
