1eadd5ea0d07e23cfdd8f727191f59bc6bf4d047
hipitihop <anonymous.amphibian@gmail.com>
2015-03-04 12:04:10 +1000

    add prismatic schema def WIP

--- examples/todomvc/src/todomvc/db.cljs
+++ examples/todomvc/src/todomvc/db.cljs
@@ -1,7 +1,24 @@
-(ns todomvc.db)
+(ns todomvc.db
+  (:require
+    [schema.core :as s :include-macros true]))
 
 
 (def default-initial-state
   {:todos   (sorted-map)        ;; todo ids are the keys (for sort)
    :showing :all                ;; one of :all :done or :active
    })
+
+
+(def schema
+  {:todos   (s/both (s/pred map?) (s/pred sorted?))
+   :showing (s/enum :all :done :active)
+   })
+
+
+(defn valid-schema?
+  [db]
+  (s/validate schema db))
+
+
+
+
