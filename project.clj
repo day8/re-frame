54da79fd00a371a30ebde73bcc6c266ffc8f1c6f
mike-thompson-day8 <mike.thompson@day8.com.au>
2015-03-13 12:30:15 +1100

    Use latest version of reagent

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2760"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [reagent "0.5.0-alpha3"]]
+                 [reagent "0.5.0"]]
   
   :profiles     {:debug {:debug true}
                  :dev   {:dependencies [[spellhouse/clairvoyant "0.0-48-gf5e59d3"]]
