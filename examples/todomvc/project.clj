7b43ca74648e2ac33618bfb3d9b776436b67f8fe
mike-thompson-day8 <mike.thompson@day8.com.au>
2015-03-13 23:30:39 +1100

    Use the latest reagent

--- examples/todomvc/project.clj
+++ examples/todomvc/project.clj
@@ -1,7 +1,7 @@
 (defproject todomvc-re-frame "0.3.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2816"]
-                 [reagent "0.5.0-alpha3"]
+                 [reagent "0.5.0"]
                  [re-frame "0.3.0"]
                  [secretary "1.2.1"]]
 
