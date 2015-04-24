c2a9a041eb2fd3d63b01c172b2422691db6b137c
mike-thompson-day8 <mike.thompson@day8.com.au>
2015-04-25 09:31:03 +1000

    Add tests for new "on-changes" middleware

--- test/re-frame/middleware.cljs
+++ test/re-frame/middleware.cljs
@@ -29,3 +29,13 @@
                    handler) db [nil])]
       (is (= (:a new-db) 
              false)))))
+
+
+(deftest on-changes
+         (let [set-a     (fn [db v] (assoc db :a v))      ;; handler
+               mid-ware  (middleware/on-changes + [:c] [:a] [:b])   ;; middleware
+               wrapped   (mid-ware set-a)]                ;; wrapped middleware
+             (is (= (wrapped {:a 0 :b 2} 0)               ;; no change in 'a'
+                    {:a 0 :b 2}))
+             (is (= ({:a 4 :b 2} 0)                       ;; 'a' changed to 0
+                    {:c 2 :a 0 :b 2}))))                  ;; 'c' is a + b
