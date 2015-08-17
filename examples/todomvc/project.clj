0f4fcd61db86b5fdc24019d9794ed451b8adacdc
Antonin Hildebrand <antonin@hildebrand.cz>
2015-08-17 10:30:18 +0200

    bump todomvc deps

--- examples/todomvc/project.clj
+++ examples/todomvc/project.clj
@@ -1,29 +1,29 @@
 (defproject todomvc-re-frame "0.4.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3208"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.107"]
                  [reagent "0.5.0"]
                  [re-frame "0.4.1"]
-                 [figwheel "0.2.6"]
+                 [figwheel "0.3.7"]
                  [secretary "1.2.3"]
                  [prismatic/schema "0.4.3"]]
 
   :plugins [[lein-cljsbuild "1.0.5"]
-            [lein-figwheel "0.2.6"]]
+            [lein-figwheel "0.3.7"]]
 
   :hooks [leiningen.cljsbuild]
 
-  :profiles {:dev {:cljsbuild
-                   {:builds {:client {:source-paths ["devsrc"]
-                                      :compiler     {:main todomvc.dev
-                                                     :asset-path "js"
-                                                     :optimizations :none
-                                                     :source-map true
-                                                     :source-map-timestamp true}}}}}
+  :profiles {:dev  {:cljsbuild
+                    {:builds {:client {:source-paths ["devsrc"]
+                                       :compiler     {:main                 todomvc.dev
+                                                      :asset-path           "js"
+                                                      :optimizations        :none
+                                                      :source-map           true
+                                                      :source-map-timestamp true}}}}}
 
              :prod {:cljsbuild
-                    {:builds {:client {:compiler    {:optimizations :advanced
-                                                     :elide-asserts true
-                                                     :pretty-print false}}}}}}
+                    {:builds {:client {:compiler {:optimizations :advanced
+                                                  :elide-asserts true
+                                                  :pretty-print  false}}}}}}
 
   :figwheel {:server-port 3450
              :repl        false}
