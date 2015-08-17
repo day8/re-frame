c05223fef8b9a90148eaa814b4670deb70e85dd8
Antonin Hildebrand <antonin@hildebrand.cz>
2015-08-17 12:07:50 +0200

    default loggers should apply all args

--- src/re_frame/logging.cljs
+++ src/re_frame/logging.cljs
@@ -2,6 +2,24 @@
 
 (defn no-op [& _])
 
+(defn js-console-log [& args]
+  (.apply (.-log js/console) js/console (into-array args)))
+
+(defn js-console-warn [& args]
+  (.apply (.-warn js/console) js/console (into-array args)))
+
+(defn js-console-error [& args]
+  (.apply (.-error js/console) js/console (into-array args)))
+
+(defn js-console-group [& args]
+  (if (.-group js/console)                                  ;; group does not exist  < IE 11
+    (.apply (.-group js/console) js/console (into-array args))
+    (apply js-console-log args)))
+
+(defn js-console-group-end [& args]
+  (if (.-groupEnd js/console)                               ;; groupEnd does not exist  < IE 11
+    (.apply (.-groupEnd js/console) js/console (into-array args))))
+
 ;; -- Logging -----------------------------------------------------------------
 ;;
 ;; re-frame internally uses a set of logging functions which, by default,
@@ -12,16 +30,15 @@
 ;; handled by overridding "error"
 ;;
 (def default-loggers
-  {:log       #(.log js/console %)
-   :warn      #(.warn js/console %)
-   :error     #(.error js/console %)
-   :group     #(if (.group js/console) (.group js/console %) (.log js/console %))  ;; group does not exist  < IE 11
-   :groupEnd  #(when (.groupEnd js/console) (.groupEnd js/console))})              ;; groupEnd does not exist  < IE 11
+  {:log      js-console-log
+   :warn     js-console-warn
+   :error    js-console-error
+   :group    js-console-group
+   :groupEnd js-console-group-end})
 
 (def no-loggers
-  {:log       no-op
-   :warn      no-op
-   :error     no-op
-   :group     no-op
-   :groupEnd  no-op}
-)
+  {:log      no-op
+   :warn     no-op
+   :error    no-op
+   :group    no-op
+   :groupEnd no-op})
