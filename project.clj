8b796c3d1910fb82d47cbfbb993021d8c21cdbc7
Daniel Compton <desk@danielcompton.net>
2015-08-26 20:56:41 +1200

    Update version to 0.5.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-frame "0.4.1"
+(defproject re-frame "0.5.0-SNAPSHOT"
   :description  "A Clojurescript MVC-like Framework For Writing SPAs Using Reagent."
   :url          "https://github.com/Day8/re-frame.git"
   :license      {:name "MIT"}
