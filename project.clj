96ff91f1ff6012f9d9953f59d75b6a277e179b7a
Daniel Compton <desk@danielcompton.net>
2016-01-28 23:20:57 +1300

    Bump project to 0.7.0-alpha2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-frame "0.7.0-alpha"
+(defproject re-frame "0.7.0-alpha-2"
   :description  "A Clojurescript MVC-like Framework For Writing SPAs Using Reagent."
   :url          "https://github.com/Day8/re-frame.git"
   :license      {:name "MIT"}
