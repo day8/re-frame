acbfda41b3bd41b6680f2e3947fbf68a2c32e876
Mike Thompson <mike.thompson@day8.com.au>
2016-07-24 18:15:02 +1000

    v0.0.9-alpha4

--- examples/todomvc/project.clj
+++ examples/todomvc/project.clj
@@ -2,7 +2,7 @@
   :dependencies [[org.clojure/clojure        "1.8.0"]
                  [org.clojure/clojurescript  "1.9.89"]
                  [reagent "0.6.0-rc"]
-                 [re-frame "0.8.0-alpha3"]
+                 [re-frame "0.8.0-alpha4"]
                  [binaryage/devtools "0.7.0"]
                  [secretary "1.2.3"]]
 
