999d4c332ed31a4e2e907a1b61a8b6b5b124a87c
Stuart Mitchell <stu@stuartmitchell.com>
2016-12-08 12:22:08 +1300

    Example - changed figwheel config

--- examples/simple/devsrc/simple/dev.cljs
+++
@@ -1,7 +0,0 @@
-(ns simple.dev
-  (:require [simple.core :as example]
-            [figwheel.client :as fw]))
-
-(fw/start {:on-jsload example/run
-           :websocket-url "ws://localhost:3449/figwheel-ws"})
-
