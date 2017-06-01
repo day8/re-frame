599b1374af77fb50ad9439a5609303624c9fc881
Stuart Mitchell <github@stuartmitchell.com>
2017-06-01 16:38:32 +1200

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-frame "0.9.4"
+(defproject re-frame "0.9.5-SNAPSHOT"
   :description  "A Clojurescript MVC-like Framework For Writing SPAs Using Reagent."
   :url          "https://github.com/Day8/re-frame.git"
   :license      {:name "MIT"}
