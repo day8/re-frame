2ce934635c2f034bf12e0dcda9138d51944e0c02
Christophe Gragnic <cgragnic@protonmail.com>
2017-07-14 07:14:55 +0200

    Fix my fix 'only one instance'

--- examples/simple/src/simple/core.cljs
+++ examples/simple/src/simple/core.cljs
@@ -13,7 +13,7 @@
     (rf/dispatch [:timer now])))  ;; <-- dispatch used
 
 ;; Call the dispatching function every second.
-;; `defonce` is like `def` but it ensures only instance one is ever
+;; `defonce` is like `def` but it ensures only one instance is ever
 ;; created in the face of figwheel hot-reloading of this file.
 (defonce do-timer (js/setInterval dispatch-timer-event 1000))
 
