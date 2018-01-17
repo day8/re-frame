day8/re-frame

commit 64f858c4e33f68b1da5d6f9532defd8a73345b0b

Trace the reagent-id of the reactive signals, not their dereffed values
This code used to get the reagent-id for every derefed value of the
input-signals, which returned all nils. This change traces the reactive
versions of the input signals.

Fixes https://github.com/Day8/re-frame-trace/issues/83

src/re_frame/subs.cljc
@@ -168,16 +168,30 @@
         (map (fn [[k v]] [k (f v)]))
         m))
 
+(defn map-signals
+  "Runs f over signals. Signals may take several
+  forms, this function handles all of them."
+  [f signals]
+  (cond
+    (sequential? signals) (map f signals)
+    (map? signals) (map-vals f signals)
+    (deref? signals) (f signals)
+    :else nil))
+
+(defn to-seq
+  "Coerces x to a seq if it isn't one already"
+  [x]
+  (if (sequential? x)
+    x
+    (list x)))
 
 (defn- deref-input-signals
   [signals query-id]
-  (let [signals (cond
-                  (sequential? signals) (map deref signals)
-                  (map? signals) (map-vals deref signals)
-                  (deref? signals) @signals
-                  :else (console :error "re-frame: in the reg-sub for " query-id ", the input-signals function returns: " signals))]
-    (trace/merge-trace! {:tags {:input-signals (map reagent-id signals)}})
-    signals))
+  (let [dereffed-signals (map-signals deref signals)]
+    (when (nil? dereffed-signals)
+      (console :error "re-frame: in the reg-sub for " query-id ", the input-signals function returns: " signals))
+    (trace/merge-trace! {:tags {:input-signals (doall (to-seq (map-signals reagent-id signals)))}})
+    dereffed-signals))
 
 
 (defn reg-sub

src/re_frame/interop.clj
@@ -88,4 +88,4 @@
 (defn reagent-id
   "Doesn't make sense in a Clojure context currently."
   [reactive-val]
-  nil)
+  "rx-clj")

commit 1e82e55aa083ffac3016f54fac744ffcb417e557

Fixup incorrect console error from deref-input-signals
Introduced in 64f858c4e33f68b1da5d6f9532defd8a73345b0b

src/re_frame/subs.cljc
@@ -176,7 +176,7 @@
     (sequential? signals) (map f signals)
     (map? signals) (map-vals f signals)
     (deref? signals) (f signals)
-    :else nil))
+    :else '()))
 
 (defn to-seq
   "Coerces x to a seq if it isn't one already"
@@ -188,8 +188,11 @@
 (defn- deref-input-signals
   [signals query-id]
   (let [dereffed-signals (map-signals deref signals)]
-    (when (nil? dereffed-signals)
-      (console :error "re-frame: in the reg-sub for " query-id ", the input-signals function returns:" signals))
+    (cond
+      (sequential? signals) (map deref signals)
+      (map? signals) (map-vals deref signals)
+      (deref? signals) (deref signals)
+      :else (console :error "re-frame: in the reg-sub for" query-id ", the input-signals function returns:" signals))
     (trace/merge-trace! {:tags {:input-signals (doall (to-seq (map-signals reagent-id signals)))}})
     dereffed-signals))
 