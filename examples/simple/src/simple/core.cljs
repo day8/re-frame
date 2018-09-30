baf4ece9e209a488ffb4628814f72a224479bf2a
Alan Thompson <cloojure@gmail.com>
2018-09-30 12:10:44 -0700

    misc

--- examples/simple/src/simple/core.cljs
+++ examples/simple/src/simple/core.cljs
@@ -3,62 +3,56 @@
             [re-frame.core :as rf]
             [clojure.string :as str]))
 
-;; A detailed walk-through of this source code is provided in the docs:
-;; https://github.com/Day8/re-frame/blob/master/docs/CodeWalkthrough.md
+; A detailed walk-through of this source code is provided in the docs:
+; https://github.com/Day8/re-frame/blob/master/docs/CodeWalkthrough.md
 
-;; -- Domino 1 - Event Dispatch -----------------------------------------------
-
+; -- Domino 1 - Event Dispatch -----------------------------------------------
 (defn dispatch-timer-event
   []
   (let [now (js/Date.)]
-    (rf/dispatch [:timer now])))  ;; <-- dispatch used
+    (rf/dispatch [:timer-event now])))  ; <-- dispatch used
 
-;; Call the dispatching function every second.
-;; `defonce` is like `def` but it ensures only one instance is ever
-;; created in the face of figwheel hot-reloading of this file.
+; Call the dispatching function every second.
+; `defonce` is like `def` but it ensures only one instance is ever
+; created in the face of figwheel hot-reloading of this file.
 (defonce do-timer (js/setInterval dispatch-timer-event 1000))
 
-
-;; -- Domino 2 - Event Handlers -----------------------------------------------
-
-(rf/reg-event-db              ;; sets up initial application state
-  :initialize                 ;; usage:  (dispatch [:initialize])
-  (fn [_ _]                   ;; the two parameters are not important here, so use _
-    {:time (js/Date.)         ;; What it returns becomes the new application state
-     :time-color "#f88"}))    ;; so the application state will initially be a map with two keys
+; -- Domino 2 - Event Handlers -----------------------------------------------
+(rf/reg-event-db              ; sets up initial application state
+  :initialize-event                 ; usage:  (dispatch [:initialize])
+  (fn [_ _]                   ; the two parameters are not important here, so use _
+    {:time (js/Date.)         ; What it returns becomes the new application state
+     :time-color "#f88"}))    ; so the application state will initially be a map with two keys
 
 
-(rf/reg-event-db                ;; usage:  (dispatch [:time-color-change 34562])
-  :time-color-change            ;; dispatched when the user enters a new colour into the UI text field
-  (fn [db [_ new-color-value]]  ;; -db event handlers given 2 parameters:  current application state and event (a vector)
-    (assoc db :time-color new-color-value)))   ;; compute and return the new application state
+(rf/reg-event-db                ; usage:  (dispatch [:time-color-change 34562])
+  :time-color-change-event      ; dispatched when the user enters a new colour into the UI text field
+  (fn [db [_ new-color-value]]  ; -db event handlers given 2 parameters:  current application state and event (a vector)
+    (assoc db :time-color new-color-value)))   ; compute and return the new application state
 
 
-(rf/reg-event-db                 ;; usage:  (dispatch [:timer a-js-Date])
-  :timer                         ;; every second an event of this kind will be dispatched
-  (fn [db [_ new-time]]          ;; note how the 2nd parameter is destructured to obtain the data value
-    (assoc db :time new-time)))  ;; compute and return the new application state
+(rf/reg-event-db                 ; usage:  (dispatch [:timer a-js-Date])
+  :timer-event                   ; every second an event of this kind will be dispatched
+  (fn [db [_ new-time]]          ; note how the 2nd parameter is destructured to obtain the data value
+    (assoc db :time new-time)))  ; compute and return the new application state
 
 
-;; -- Domino 4 - Query  -------------------------------------------------------
+; -- Domino 4 - Query  -------------------------------------------------------
+(rf/reg-sub
+  :time-query
+  (fn [db _]     ; db is current app state. 2nd unused param is query vector
+    (:time db))) ; return a query computation over the application state
 
 (rf/reg-sub
-  :time
-  (fn [db _]     ;; db is current app state. 2nd unused param is query vector
-    (:time db))) ;; return a query computation over the application state
-
-(rf/reg-sub
-  :time-color
+  :time-color-query
   (fn [db _]
     (:time-color db)))
 
-
-;; -- Domino 5 - View Functions ----------------------------------------------
-
+; -- Domino 5 - View Functions ----------------------------------------------
 (defn clock
   []
   [:div.example-clock
-   {:style {:color @(rf/subscribe [:time-color])}}
+   {:style {:color @(rf/subscribe [:time-color-query])}}
    (-> @(rf/subscribe [:time])
        .toTimeString
        (str/split " ")
@@ -69,8 +63,8 @@
   [:div.color-input
    "Time color: "
    [:input {:type "text"
-            :value @(rf/subscribe [:time-color])
-            :on-change #(rf/dispatch [:time-color-change (-> % .-target .-value)])}]])  ;; <---
+            :value @(rf/subscribe [:time-color-query])
+            :on-change #(rf/dispatch [:time-color-change-event (-> % .-target .-value)])}]])  ; <---
 
 (defn ui
   []
@@ -79,10 +73,9 @@
    [clock]
    [color-input]])
 
-;; -- Entry Point -------------------------------------------------------------
-
+; -- Entry Point -------------------------------------------------------------
 (defn ^:export run
   []
-  (rf/dispatch-sync [:initialize])     ;; puts a value into application state
-  (reagent/render [ui]              ;; mount the application's ui into '<div id="app" />'
+  (rf/dispatch-sync [:initialize-event])     ; puts a value into application state
+  (reagent/render [ui]              ; mount the application's ui into '<div id="app" />'
                   (js/document.getElementById "app")))
--- examples/todomvc/src/todomvc/db.cljs
+++ examples/todomvc/src/todomvc/db.cljs
@@ -3,85 +3,81 @@
             [cljs.spec.alpha :as s]
             [re-frame.core :as re-frame]))
 
-
-;; -- Spec --------------------------------------------------------------------
-;;
-;; This is a clojure.spec specification for the value in app-db. It is like a
-;; Schema. See: http://clojure.org/guides/spec
-;;
-;; The value in app-db should always match this spec. Only event handlers
-;; can change the value in app-db so, after each event handler
-;; has run, we re-check app-db for correctness (compliance with the Schema).
-;;
-;; How is this done? Look in events.cljs and you'll notice that all handlers
-;; have an "after" interceptor which does the spec re-check.
-;;
-;; None of this is strictly necessary. It could be omitted. But we find it
-;; good practice.
+; -- Spec --------------------------------------------------------------------
+;
+; This is a clojure.spec specification for the value in app-db. It is like a
+; Schema. See: http://clojure.org/guides/spec
+;
+; The value in app-db should always match this spec. Only event handlers
+; can change the value in app-db so, after each event handler
+; has run, we re-check app-db for correctness (compliance with the Schema).
+;
+; How is this done? Look in events.cljs and you'll notice that all handlers
+; have an "after" interceptor which does the spec re-check.
+;
+; None of this is strictly necessary. It could be omitted. But we find it
+; good practice.
 
 (s/def ::id int?)
 (s/def ::title string?)
 (s/def ::done boolean?)
 (s/def ::todo (s/keys :req-un [::id ::title ::done]))
-(s/def ::todos (s/and                                       ;; should use the :kind kw to s/map-of (not supported yet)
-                 (s/map-of ::id ::todo)                     ;; in this map, each todo is keyed by its :id
-                 #(instance? PersistentTreeMap %)           ;; is a sorted-map (not just a map)
-                 ))
-(s/def ::showing                                            ;; what todos are shown to the user?
-  #{:all                                                    ;; all todos are shown
-    :active                                                 ;; only todos whose :done is false
-    :done                                                   ;; only todos whose :done is true
-    })
+(s/def ::todos (s/and                                       ; should use the :kind kw to s/map-of (not supported yet)
+                 (s/map-of ::id ::todo)                     ; in this map, each todo is keyed by its :id
+                 #(instance? PersistentTreeMap %)))         ; is a sorted-map (not just a map)
+
+(s/def ::showing                                            ; what todos are shown to the user?
+  #{:all                                                    ; all todos are shown
+    :active                                                 ; only todos whose :done is false
+    :done})                                                 ; only todos whose :done is true
+
 (s/def ::db (s/keys :req-un [::todos ::showing]))
 
-;; -- Default app-db Value  ---------------------------------------------------
-;;
-;; When the application first starts, this will be the value put in app-db
-;; Unless, of course, there are todos in the LocalStore (see further below)
-;; Look in:
-;;   1.  `core.cljs` for  "(dispatch-sync [:initialise-db])"
-;;   2.  `events.cljs` for the registration of :initialise-db handler
-;;
+; -- Default app-db Value  ---------------------------------------------------
+;
+; When the application first starts, this will be the value put in app-db
+; Unless, of course, there are todos in the LocalStore (see further below)
+; Look in:
+;   1.  `core.cljs` for  "(dispatch-sync [:initialise-db])"
+;   2.  `events.cljs` for the registration of :initialise-db handler
 
-(def default-db           ;; what gets put into app-db by default.
-  {:todos   (sorted-map)  ;; an empty list of todos. Use the (int) :id as the key
-   :showing :all})        ;; show all todos
+(def default-db           ; what gets put into app-db by default.
+  {:todos   (sorted-map)  ; an empty list of todos. Use the (int) :id as the key
+   :showing :all})        ; show all todos
 
 
-;; -- Local Storage  ----------------------------------------------------------
-;;
-;; Part of the todomvc challenge is to store todos in LocalStorage, and
-;; on app startup, reload the todos from when the program was last run.
-;; But the challenge stipulates to NOT load the setting for the "showing"
-;; filter. Just the todos.
-;;
+; -- Local Storage  ----------------------------------------------------------
+;
+; Part of the todomvc challenge is to store todos in LocalStorage, and
+; on app startup, reload the todos from when the program was last run.
+; But the challenge stipulates to NOT load the setting for the "showing"
+; filter. Just the todos.
 
-(def ls-key "todos-reframe")                         ;; localstore key
+(def ls-key "todos-reframe")                         ; localstore key
 
 (defn todos->local-store
   "Puts todos into localStorage"
   [todos]
-  (.setItem js/localStorage ls-key (str todos)))     ;; sorted-map written as an EDN map
+  (.setItem js/localStorage ls-key (str todos)))     ; sorted-map written as an EDN map
 
+; -- cofx Registrations  -----------------------------------------------------
 
-;; -- cofx Registrations  -----------------------------------------------------
-
-;; Use `reg-cofx` to register a "coeffect handler" which will inject the todos
-;; stored in localstore.
-;;
-;; To see it used, look in `events.cljs` at the event handler for `:initialise-db`.
-;; That event handler has the interceptor `(inject-cofx :local-store-todos)`
-;; The function registered below will be used to fulfill that request.
-;;
-;; We must supply a `sorted-map` but in LocalStore it is stored as a `map`.
-;;
+; Use `reg-cofx` to register a "coeffect handler" which will inject the todos
+; stored in localstore.
+;
+; To see it used, look in `events.cljs` at the event handler for `:initialise-db`.
+; That event handler has the interceptor `(inject-cofx :local-store-todos)`
+; The function registered below will be used to fulfill that request.
+;
+; We must supply a `sorted-map` but in LocalStore it is stored as a `map`.
+;
 (re-frame/reg-cofx
   :local-store-todos
   (fn [cofx _]
-      ;; put the localstore todos into the coeffect under :local-store-todos
+      ; put the localstore todos into the coeffect under :local-store-todos
       (assoc cofx :local-store-todos
-             ;; read in todos from localstore, and process into a sorted map
+             ; read in todos from localstore, and process into a sorted map
              (into (sorted-map)
                    (some->> (.getItem js/localStorage ls-key)
-                            (cljs.reader/read-string)    ;; EDN map -> map
+                            (cljs.reader/read-string)    ; EDN map -> map
                             )))))
