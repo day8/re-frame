9c3b6398e5578832ce4f662e436156f5cfba1453
Isaac Johnston <isaac@superstruct.nz>
2019-09-27 00:33:03 +0800

    Upgrade todomvc example shadow-cljs to 2.8.59

--- examples/todomvc/project.clj
+++ examples/todomvc/project.clj
@@ -3,7 +3,7 @@
                  [org.clojure/clojurescript  "1.10.520"
                   :exclusions [com.google.javascript/closure-compiler-unshaded
                                org.clojure/google-closure-library]]
-                 [thheller/shadow-cljs "2.8.52"]
+                 [thheller/shadow-cljs "2.8.59"]
                  [reagent "0.9.0-rc1"]
                  [re-frame "0.11.0-rc2-SNAPSHOT"]
                  [binaryage/devtools "0.9.10"]
