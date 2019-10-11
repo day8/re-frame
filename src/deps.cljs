ca83567ef3284e3ce0b2917413b16cd72883588e
Isaac Johnston <isaac@superstruct.nz>
2019-10-11 15:45:26 +0800

    Upgrade karma-junit-reporter to 2.0.1

--- src/deps.cljs
+++ src/deps.cljs
@@ -2,4 +2,4 @@
                 "karma"                 "4.3.0"
                 "karma-chrome-launcher" "3.1.0"
                 "karma-cljs-test"       "0.1.0"
-                "karma-junit-reporter"  "1.2.0"}}
+                "karma-junit-reporter"  "2.0.1"}}
