a7d4f0c8e2096aa5954f009d7b05cb350322a5dc
Isaac Johnston <isaac@superstruct.nz>
2019-10-11 15:44:45 +0800

    Upgrade shadow-cljs to 2.8.62

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
                  [org.clojure/clojurescript "1.10.520" :scope "provided"
                   :exclusions [com.google.javascript/closure-compiler-unshaded
                                org.clojure/google-closure-library]]
-                 [thheller/shadow-cljs "2.8.61" :scope "provided"]
+                 [thheller/shadow-cljs "2.8.62" :scope "provided"]
                  [reagent "0.9.0-rc1"]
                  [net.cgrand/macrovich "0.2.1"]
                  [org.clojure/tools.logging "0.4.1"]]
--- src/deps.cljs
+++ src/deps.cljs
@@ -1,4 +1,4 @@
-{:npm-dev-deps {"shadow-cljs"           "2.8.61"
+{:npm-dev-deps {"shadow-cljs"           "2.8.62"
                 "karma"                 "4.3.0"
                 "karma-chrome-launcher" "3.1.0"
                 "karma-cljs-test"       "0.1.0"
