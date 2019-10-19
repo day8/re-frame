e6289c28386119286ad681e85cb2d78baeb15b80
Isaac Johnston <isaac@superstruct.nz>
2019-10-19 17:32:57 +0800

    Upgrade karma to 4.4.1

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,5 +1,5 @@
 {:npm-dev-deps {"shadow-cljs"           "2.8.64"
-                "karma"                 "4.3.0"
+                "karma"                 "4.4.1"
                 "karma-chrome-launcher" "3.1.0"
                 "karma-cljs-test"       "0.1.0"
                 "karma-junit-reporter"  "2.0.1"}}
