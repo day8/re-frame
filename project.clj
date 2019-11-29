b65521c283ab7be05aeec629f23c4b379d7877ff
Isaac Johnston <isaac@superstruct.nz>
2019-11-30 00:53:38 +0700

    Add org.clojure/google-closure-library-third-party exclusion

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure       "1.10.1"   :scope "provided"]
                  [org.clojure/clojurescript "1.10.520" :scope "provided"
                   :exclusions [com.google.javascript/closure-compiler-unshaded
-                               org.clojure/google-closure-library]]
+                               org.clojure/google-closure-library
+                               org.clojure/google-closure-library-third-party]]
                  [thheller/shadow-cljs      "2.8.69"   :scope "provided"]
                  [reagent                   "0.9.0-rc3"]
                  [net.cgrand/macrovich      "0.2.1"]
