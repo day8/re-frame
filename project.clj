b35fbe6037754161c0175ce7e6742ce330334e57
Isaac Johnston <isaac@superstruct.nz>
2019-11-30 00:58:40 +0700

    Upgrade lein-shadow to 0.1.7

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
                  [org.clojure/tools.logging "0.4.1"]]
 
   :plugins      [[day8/lein-git-inject "0.0.2"]
-                 [lein-shadow          "0.1.6"]]
+                 [lein-shadow          "0.1.7"]]
 
   :middleware   [leiningen.git-inject/middleware]
 
