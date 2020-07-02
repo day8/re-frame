3ac3cd99fa3247294c54b33ea485ba67575a48c9
Isaac Johnston <isaac@superstruct.nz>
2020-07-02 20:07:00 +1200

    Change Clojars password to token

--- project.clj
+++ project.clj
@@ -44,7 +44,7 @@
   :deploy-repositories [["clojars" {:sign-releases false
                                     :url "https://clojars.org/repo"
                                     :username :env/CLOJARS_USERNAME
-                                    :password :env/CLOJARS_PASSWORD}]]
+                                    :password :env/CLOJARS_TOKEN}]]
 
   :release-tasks [["deploy" "clojars"]]
 
