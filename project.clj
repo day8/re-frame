279eee5475201682868f0be5e523befd27bf5f06
Isaac Johnston <isaac@superstruct.nz>
2020-07-24 11:59:02 +1200

    Fix package.json issues
    Fixes potential 'Cannot find local Karma!' error on GitHub Actions

--- project.clj
+++ project.clj
@@ -70,10 +70,27 @@
                           :compiler-options {:pretty-print                       true
                                              :closure-defines                    {re-frame.trace.trace-enabled? true}}}}}
 
-  :aliases {"test-once"   ["do" "clean," "shadow" "compile" "browser-test," "shell" "open" "run/compiled/browser/test/index.html"]
-            "test-auto"   ["do" "clean," "shadow" "watch" "browser-test,"]
+  ;; The git update-index command is required to ignore changes to package.json as
+  ;; 1. package.json must be committed to the repo for npm install --save... to behave correctly, which is used by
+  ;;    lein-shadow to install dependencies that would cause the build to fail if missing; e.g. karma
+  ;; 2. .gitignore does nothing for files that are already committed
+  ;; 3. git recognising package.json modifications would cause day8/lein-git-inject to always incorrectly use
+  ;;    -SNAPSHOT versions.
+  :aliases {"test-once"   ["do"
+                           ["clean"]
+                           ["shell" "git" "update-index" "--assume-unchanged" "package.json"]
+                           ["shadow" "compile" "browser-test"]
+                           ["shell" "open" "run/compiled/browser/test/index.html"]]
+            "test-auto"   ["do"
+                           ["clean"]
+                           ["shell" "git" "update-index" "--assume-unchanged" "package.json"]
+                           ["shadow" "watch" "browser-test"]]
             "karma-once"  ["do"
                            ["clean"]
+                           ["shell" "git" "update-index" "--assume-unchanged" "package.json"]
                            ["shadow" "compile" "karma-test"]
                            ["shell" "karma" "start" "--single-run" "--reporters" "junit,dots"]]
-            "karma-auto"  ["do" "clean," "shadow" "watch" "karma-test,"]})
+            "karma-auto"  ["do"
+                           ["clean"]
+                           ["shell" "git" "update-index" "--assume-unchanged" "package.json"]
+                           ["shadow" "watch" "karma-test"]]})
