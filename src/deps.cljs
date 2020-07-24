f119f3d6d294243c6a9c15dfad8fcb7dad26951f
Isaac Johnston <isaac@superstruct.nz>
2020-07-24 12:04:38 +1200

    Upgrade karma to 5.1.0

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,5 +1,5 @@
 {:npm-dev-deps {"shadow-cljs"              "2.10.17"
-                "karma"                    "4.4.1"
+                "karma"                    "5.1.0"
                 "karma-phantomjs-launcher" "1.0.4"
                 "karma-chrome-launcher"    "3.1.0"
                 "karma-cljs-test"          "0.1.0"
