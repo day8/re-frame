cd49ebd391693867ea483b6ff3492e6efcc47568
Isaac Johnston <isaac@superstruct.nz>
2020-08-29 01:06:45 +1200

    Revert "Add styling to api docs"
    Introduced some styling bugs related to `:focus` styles on headings.
    
    This reverts commit 0ca10b891a5bf320bf9d6e77e479683f4acb7cd9.

--- docs/src/ns_to_markdown.clj
+++ docs/src/ns_to_markdown.clj
@@ -88,12 +88,9 @@
     (catch Exception e
       (println e))))
 
-(def markdown-styles
-  "<style>\n.md-typeset h3 {\nbackground: #777;\ncolor: white;\npadding-left: 5px;\nline-height: 1.15em;\nheight: 2em;\n}\n</style>\n\n")
-
 (defn ns->markdown
   [m]
-  (format "%s# %s\n\n" markdown-styles (:name m)))
+  (format "# %s\n\n" (:name m)))
 
 (defn arglist->markdown
   [arglist]
