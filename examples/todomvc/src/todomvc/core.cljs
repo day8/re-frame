5238bf99e4be50cb3b66a78ee13085c175350c1e
Milosz Danczak <milosz.danczak@gmail.com>
2021-01-24 14:27:28 +0100

    Fix a warning in todomvc
    Add a type hint to fix `Cannot infer target type in expression (. event -token)`.

--- examples/todomvc/src/todomvc/core.cljs
+++ examples/todomvc/src/todomvc/core.cljs
@@ -36,7 +36,7 @@
 (defonce history
   (doto (History.)
     (events/listen EventType.NAVIGATE
-                   (fn [event] (secretary/dispatch! (.-token event))))
+                   (fn [^js/goog.History.Event event] (secretary/dispatch! (.-token event))))
     (.setEnabled true)))
 
 
