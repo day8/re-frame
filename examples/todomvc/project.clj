af065b73cf6c54fa6d327f2452eaad80a02c83de
Isaac Johnston <isaac@superstruct.nz>
2021-02-25 00:39:31 +1300

    Upgrade example dependencies

--- examples/todomvc/project.clj
+++ examples/todomvc/project.clj
@@ -1,23 +1,24 @@
 (defproject todomvc-re-frame "lein-git-inject/version"
 
-  :dependencies [[org.clojure/clojure        "1.10.1"]
+  :dependencies [[org.clojure/clojure        "1.10.2"]
                  [org.clojure/clojurescript  "1.10.773"
                   :exclusions [com.google.javascript/closure-compiler-unshaded
                                org.clojure/google-closure-library
                                org.clojure/google-closure-library-third-party]]
-                 [thheller/shadow-cljs       "2.11.4"]
+                 [thheller/shadow-cljs       "2.11.18"]
                  ;; We repeat re-frame's own dependencies here as instead of
                  ;; depending on a re-frame artifact we add the re-frame source
                  ;; from this repository directly to the :source-paths.
                  [reagent                    "1.0.0"]
                  [net.cgrand/macrovich       "0.2.1"]
-                 [org.clojure/tools.logging  "0.4.1"]
+                 [org.clojure/tools.logging  "1.1.0"]
                  [binaryage/devtools         "1.0.2"]
                  [clj-commons/secretary      "1.2.4"]
                  [day8.re-frame/tracing      "0.6.0"]]
 
   :plugins      [[day8/lein-git-inject "0.0.14"]
-                 [lein-shadow          "0.3.1"]]
+                 [lein-shadow          "0.3.1"]
+                 [lein-ancient         "0.6.15"]]
 
   :middleware   [leiningen.git-inject/middleware]
 
--- examples/todomvc/src/deps.cljs
+++ examples/todomvc/src/deps.cljs
@@ -1,1 +1,1 @@
-{:npm-dev-deps {"shadow-cljs" "2.11.4"}}
+{:npm-dev-deps {"shadow-cljs" "2.11.18"}}
