a79fd6d1b67b81ca67ae297725033b60610e28f9
Isaac Johnston <isaac@superstruct.nz>
2021-11-04 12:56:52 +1300

    Upgrade shadow-cljs to 2.15.12 and ClojureScript to 1.10.879

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,5 +1,5 @@
-{:npm-dev-deps {"shadow-cljs"              "2.14.0"
-                "karma"                    "6.3.0"
+{:npm-dev-deps {"shadow-cljs"              "2.15.12"
+                "karma"                    "6.3.7"
                 "karma-phantomjs-launcher" "1.0.4"
                 "karma-chrome-launcher"    "3.1.0"
                 "karma-cljs-test"          "0.1.0"
