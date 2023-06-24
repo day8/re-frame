80f3049f422e2f48dd84c793676a9ab69b8268b8
Kimo Knowles <kimo.knowles@day8.com.au>
2023-06-24 14:43:25 +0200

    [cleanup] Remove klipse
    codemirror.css was a theme for the codemirror 5 elements invoked by
    klipse. It doesn't apply to the new codemirror 6 elements, which use
    a different dom tree.
    
    Removed codemirror.css for now. We might consider porting this
    stylesheet, or finding out a new theme.

--- docs/klipse/build-cljs-cache-for-lib-namespaces.cljs
+++
@@ -1,8 +0,0 @@
-;; This file only exists to require the namespaces that we want planck to compile into cache files.
-(ns klipse.generate-lib-cljs-cache
-  (:require
-    [klipse-clj.repl]
-    [klipse-clj.macros]
-    [react]
-    [reagent.core]
-    [re-frame.core]))
