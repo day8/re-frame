day8/re-frame

commit fd1b12a30055876269380b75d37472cfb5d32346

[feature] Support an optional :comment in an interceptor map
Now, we can define an interceptor which comments on itself. This is
useful to https://github.com/day8/re-frame-10x/issues/165, for
instance, where our :path interceptor should describe what path it's
actually going to.

In the future, we should consider a more permissive spec. Require
fully-qualified :before & :after keys, and don't exclude
anything. I think this would better match Clojure's design
principles. The warning mechanism could just check for weird keys like
misspellings or unqualified :before & :after keys.

src/re_frame/interceptor.cljc
@@ -8,22 +8,26 @@
 
 (def mandatory-interceptor-keys #{:id :after :before})
 
+(def optional-interceptor-keys #{:comment})
+
 (defn interceptor?
   [m]
   (and (map? m)
-       (= mandatory-interceptor-keys (-> m keys set))))
-
+       (= mandatory-interceptor-keys
+          (-> m keys set (set/difference optional-interceptor-keys)))))
 
 (defn ->interceptor
   [& {:as m :keys [id before after]}]
   (when debug-enabled?
     (if-let [unknown-keys (seq (set/difference
                                 (-> m keys set)
-                                mandatory-interceptor-keys))]
+                                mandatory-interceptor-keys
+                                optional-interceptor-keys))]
       (console :error "re-frame: ->interceptor" m "has unknown keys:" unknown-keys)))
-  {:id     (or id :unnamed)
-   :before before
-   :after  after})
+  (cond-> {:id     (or id :unnamed)
+           :before before
+           :after  after}
+    comment (assoc :comment comment)))
 
 ;; -- Effect Helpers  -----------------------------------------------------------------------------
 

commit 06d08f95d93ebaee2952a08f72f34c02db582dfb

Fix interceptor comment key
fixes fd1b12a30055876269380b75d37472cfb5d32346, which accidentally
references clojure.core/comment instead of the interceptor :comment
key.

src/re_frame/interceptor.cljc
@@ -17,7 +17,7 @@
           (-> m keys set (set/difference optional-interceptor-keys)))))
 
 (defn ->interceptor
-  [& {:as m :keys [id before after]}]
+  [& {:as m :keys [id comment before after]}]
   (when debug-enabled?
     (if-let [unknown-keys (seq (set/difference
                                 (-> m keys set)