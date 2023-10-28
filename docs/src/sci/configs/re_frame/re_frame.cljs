389c7b1c05c2795912169e9104609daa276f9a49
Kimo Knowles <kimo.knowles@day8.com.au>
2023-10-28 18:39:00 +0200

    [docs] Support re-frame.alpha

--- docs/src/sci/configs/re_frame/re_frame.cljs
+++ docs/src/sci/configs/re_frame/re_frame.cljs
@@ -2,10 +2,12 @@
   (:require
    [re-frame.core]
    [re-frame.db]
+   [re-frame.alpha]
    [sci.core :as sci]))
 
 (def rfns (sci/create-ns 're-frame.core nil))
 (def rfdbns (sci/create-ns 're-frame.db nil))
+(def rfa (sci/create-ns 're-frame.alpha nil))
 
 (def re-frame-namespace
   (sci/copy-ns re-frame.core rfns))
@@ -13,9 +15,13 @@
 (def re-frame-db-namespace
   (sci/copy-ns re-frame.db rfdbns))
 
+(def re-frame-alpha-namespace
+  (sci/copy-ns re-frame.alpha rfns))
+
 (def namespaces
   {'re-frame.core re-frame-namespace
-   're-frame.db re-frame-db-namespace})
+   're-frame.db re-frame-db-namespace
+   're-frame.alpha re-frame-alpha-namespace})
 
 (def config
   {:namespaces namespaces})
